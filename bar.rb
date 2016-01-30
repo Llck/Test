7a042d0a03f50b8b217678dd823867fbf6edea00
Алина Канивец < >
2016-01-30 14:22:37 +0200

    new bar functin

--- bar.rb
+++ bar.rb
@@ -1,9 +1,11 @@
 def chus  (a) 
-	
-	if (a>100) return -1
-		if (a<10) return 0
+	m=a*3
+	if a>100
+	puts "-1" 
+		if a<10
+			puts"0"
 				
-			else return a*3
+			else puts "#{m}"
 				
 			end	
 end
