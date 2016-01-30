28e35e3da861ad38064c5b65c504e708d3c0afcc
Алина Канивец < >
2016-01-30 21:25:18 +0200

    changes in  files

--- bar-test.rb
+++ bar-test.rb
@@ -6,24 +6,29 @@ class BarTest < Test::Unit::TestCase
   def setup
     ## @num = SimpleNumber.new(2)
   end
+  
   def teardown
     ## Nothing really
   end
 
 test "should return the symbol multiplied by 3 does not go beyond 10" do
 	var =9
-	assert_equal(0,chus(var))
+	    assert_equal(0,chus(var))
+
 	var =10
-	assert_equal(30,chus(var))
+	    assert_equal(30,chus(var))
+
 	var =11
-	assert_equal(33,chus(var))
+	    assert_equal(33,chus(var))
 end
 test "should return the symbol multipliedby 3 does not go beyond 100" do
     var =99
-    assert_equal(297,chus(var))
+        assert_equal(297,chus(var))
+
     var =100
-    assert_equal(300,chus(var))
+        assert_equal(300,chus(var))
+
     var =101
-   assert_equal(-1,chus(var))
+        assert_equal(-1,chus(var))
 end
 end	
--- bar.rb
+++ bar.rb
@@ -1,12 +1,13 @@
 def chus  (a) 
-	m=a*3
-	if a>100
-	return -1 
+m=a*3
+    if a>100
+	    return -1 
+
 	elsif a<10
 		return 0
-				
+
 	else
 	    return m
-				
-			end	
+
+	end	
 end
