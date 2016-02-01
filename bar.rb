193018c0f6fbde286bd506999672c9da8abf14e9
Алина Канивец < >
2016-02-01 21:17:38 +0200

    удаление всех return-ов с функции, проверка тестами

--- bar.rb
+++ bar.rb
@@ -1,17 +1,17 @@
 def chus  (a) 
     m=a*3
     if a>100
-	    return -1 
+	     -1 
 
 	elsif a<10
-		return 0
+		 0
 
 	else
-	    return m
+	     m
 
     end	
 end
 
 def chus9
-    return chus(9)
+     chus(9)
 end
