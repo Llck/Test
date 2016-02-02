16df7dd21aaa2a820ac870d79cd057f37e1c1f78
Алина Канивец < >
2016-02-02 15:16:55 +0200

    правильная логическая запись условий

--- bar.rb
+++ bar.rb
@@ -1,14 +1,14 @@
 def chus  (a) 
    
-    if a>100
+    if !(a<200)&&!(a>255)
+         a*2
+
+	elsif a>100
 	     -1 
 
-	elsif a<10
+
+    elsif a<10
 		 0
-
-
-    elsif (a>200)&&(255>a)
-         a*2
 
 	else
 	     a*3
